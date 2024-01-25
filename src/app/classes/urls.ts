159de0c28fcc5e1b66d7d71892f7afd1852aba43
JM_ARAUJO <jmaz.dev@gmail.com>
2024-01-25 15:00:25 -0300

    update url

--- src/app/classes/urls.ts
+++ src/app/classes/urls.ts
@@ -5,6 +5,6 @@ export class Urls {
     this.url_base_api = 'http://localhost:3333/api/';
 
     //URL VERCEL
-    // this.url_base_api = '';
+    // this.url_base_api = 'https://calendar-challenge-api.vercel.app/';
   }
 }
